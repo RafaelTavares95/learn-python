9510ddee19701dfd9ed1502dfcf2736d37adc0f9
Rafael Tavares <rafaeltavares.rofino198@gmail.com>
2026-01-06 11:32:14 -0300

    feat: configure logs into files

--- socialapi/logging_conf.py
+++ socialapi/logging_conf.py
@@ -13,20 +13,47 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "timed_rotating_file": {
+                    "class": "logging.handlers.TimedRotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "logs/socialapi.log",
+                    "when": "midnight",
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                    "utc": True,
+                },
             },
             "loggers": {
                 "socialapi": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "timed_rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
+                },
+                "uvicorn": {
+                    "handlers": ["default", "timed_rotating_file"],
+                    "level": "INFO",
+                },
+                "databases": {
+                    "handlers": ["default", "timed_rotating_file"],
+                    "level": "WARNING",
+                },
+                "aiosqlite": {
+                    "handlers": ["default", "timed_rotating_file"],
+                    "level": "WARNING",
                 },
             },
         }
